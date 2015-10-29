2bb9a05879c7b759e953ec97e84c468cf202b78d
thingsastray <andrewpeterson808@gmail.com>
2015-10-28 19:24:45 -1000

    Completed Test

--- basics.js
+++ basics.js
@@ -1,22 +1,41 @@
+var myName = "Andrew Peterson";
+var person =  { name: myName, age: 22};
+var age = {};
+console.log(myName);
+console.log(person);
 /* Create a `myName` variable and assign it a String value */
-
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+function greet(name){
+	 console.log("Hello, my name is " + name);
+}
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+var canDrive = (person.age >= 16);
+
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
 
+var dataTypes = [
+		"somthing", 23, false, {month: 'March'}, undefined, null];
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+var dog = {
+
+	bark: function () {
+		console.log( "bark" );
+	},
+	name: "Spot"
+};
 
 /* Create a Dog object
  * it should have a `bark` function that makes your dog bark!
